66be0eb910380a900fd829259a07f59799362caa
Pat Wilson <patw@patwsd.com>
2016-02-04 21:45:53 -0500

    new classes

---
+++ src/netMonitor.h
@@ -0,0 +1,28 @@
+
+/*
+ * netMonitor.h
+ *
+ *  Created on: 2015-11-22
+ *      Author: patw
+ */
+
+#ifndef _NETMONITOR_H_
+#define _NETMONITOR_H_
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+class NetMonitor {
+public:
+    NetMonitor();
+    ~NetMonitor();
+private:
+};
+
+#endif /* _NETMONITOR_H_ */
+
---
+++ src/sysdWatchdog.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: 2015-11-22
+ *      Author: patw
+ */
+
+#ifndef _SYSDWATCHDOG_H_
+#define _SYSDWATCHDOG_H_
+#ifdef SYSTEMD_WDOG
+#include <systemd/sd-daemon.h>
+#endif
+
+#endif /* _SYSDWATCHDOG_H_ */
+
